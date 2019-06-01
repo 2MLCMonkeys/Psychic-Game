f9a1cfffe5f1fbf42cd9ff5a686d97817c6a3518
2mlcmonkeys <2mlcmonkeys@gmail.com>
2019-06-01 12:59:03 -0400

    updating javascript game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
-var guessAletter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var guessAletter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 
 var letterGuess = document.getElementById('current-guess');
 
@@ -15,14 +17,42 @@ var scoreLoss = 0;
 var guessCounter = 10;
 
 
+var used = [ ];
+var computerLetter = guessAletter
+    [Math.floor(Math.random() * guessAletter.length)];
+    console.log(computerLetter);
 
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
     var letter = event.key;
-    var computerLetter = guessAletter
-    [Math.floor(Math.random() *
-        guessAletter.length)];
+    
 
-        if(letter === "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"){
-            letterGuess.textContent = event.key;
-        }
+    if (letter === computerLetter) {
+        scoreWin++;
+        guessCounter = 10;
+        used = [ ];
+        computerLetter = guessAletter
+        [Math.floor(Math.random() * guessAletter.length)];
+        console.log(computerLetter);
+    }
+    else {
+        guessCounter--;
+        used.push(letter);
+        
+
+    }
+    if (guessCounter === 0) {
+        scoreLoss++;
+        guessCounter = 10;
+        used = [ ];
+        computerLetter = guessAletter
+        [Math.floor(Math.random() * guessAletter.length)];
+        console.log(computerLetter);
+    }
+    letterGuess.textContent = event.key;
+    turnsLeft.textContent = guessCounter;
+    wins.textContent = scoreWin;
+    losses.textContent = scoreLoss;
+    pastGuesses.textContent = used;
+
+
 }
